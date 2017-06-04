3dc807ba81a5d3ff75540caecf5d1b75fe6ab480
Miha Kosmac <biomiha@Mihas-MacBook-Air.local>
2017-06-04 21:55:27 +0100

    End of day one.

--- TCGA data analysis.R
+++ TCGA data analysis.R
@@ -1,10 +1,25 @@
 #https://jhubiostatistics.shinyapps.io/recount/
+#http://127.0.0.1:30842/library/recount/doc/recount-quickstart.html
 library(recount) 
 library(SummarizedExperiment)
+library(tidyverse)
+library(Homo.sapiens)
+library(edgeR)
 load("~/Documents/Programming/R/TCGA/rse_gene.Rdata")
-rse_gene_se <- as(object = rse_gene, Class = "SummarizedExperiment")
-rse_gene[rowData(rse_gene)$gene_id == "ENSG00000000003.14", 1:5]
 features <- unlist(rowData(rse_gene)$symbol)
-source("https://bioconductor.org/biocLite.R")
-biocLite("recount")
-rse1 <- scale_counts(rse_gene)
+sample_metadata <- as_tibble(colData(rse_gene))
+feature_metadata <- as_tibble(rowData(rse_gene))
+rse_gene <- scale_counts(rse_gene)
+#http://leekgroup.github.io/recount-analyses/example_de/recount_SRP019936.html
+#subsample for computational speed
+set.seed(123456)
+test_RSE <- rse_gene[, sample(dim(rse_gene)[2], 500)]
+
+genes <- read_rds(path = "~/Documents/Programming/R/TCGA/TCGA/genes.rds")
+gene_annotation <- AnnotationDbi::select(Homo.sapiens, keys=genes$V2, columns=c("SYMBOL", "ENTREZID", "ENSEMBL"), 
+                keytype="ENTREZID")
+ensembl_names <- rowData(test_RSE)$symbol %>% unlist %>% names
+#select only gene coding transcripts
+test_RSE <- test_RSE[na.omit(match(x = ensembl_names, table = gene_annotation$ENSEMBL, nomatch = NULL)),]
+feature_filter <- rowMeans(assay(test_RSE))>5
+test_RSE <- test_RSE[feature_filter,]
